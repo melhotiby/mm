cf859c36e401b288ee1835e03e0a58356301d04f
Matt Elhotiby <hotiby@gmail.com>
2012-04-29 11:22:31 -0400

    adding the active record fix

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+set :application, "mm"
+set :deploy_to, "/var/rails/#{application}"
+set :keep_releases, 3
+
+set :scm, :git
+set :repository,  "git@github.com:railsdevmatt/mm.git"
+set :git_shallow_clone, 1
+set :branch, "master"
+set :use_sudo, true
+
+set :user, "deploy"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+role :app, "ec2-174-129-167-120.compute-1.amazonaws.com"
+role :web, "ec2-174-129-167-120.compute-1.amazonaws.com"
+role :db,  "ec2-174-129-167-120.compute-1.amazonaws.com", :primary => true
+
+task :staging do
+  set :application, "mm"
+  set :rails_env, "staging"
+  set :deploy_to, "/var/rails/#{application}"
+  server "ec2-174-129-167-120.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+end
+
+after 'deploy:update_code', 'deploy:symlink_db'
+before "deploy:restart", "deploy:bundle"
+
+namespace :deploy do
+  
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+  
+  desc "run bundle install and ensure all gem requirements are met"
+  task :bundle do
+    run "cd #{current_path} && bundle install  --without=test"
+  end
+  
+end
