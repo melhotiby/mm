961c64da4716365b6c915f6f9fc6c68c36ae43f5
Matt Elhotiby <hotiby@gmail.com>
2012-04-29 14:07:22 -0400

    testing unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,21 @@ namespace :deploy do
     run "cd #{current_path} && bundle install  --without=test"
   end
   
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "cd #{current_path} && kill -s USR2 `cat tmp/pids/unicorn.pid`"
+  end
+  # 
+  # desc "Start unicorn"
+  # task :start, :except => { :no_release => true } do
+  #   run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  # end
+  # 
+  # desc "Stop unicorn"
+  # task :stop, :except => { :no_release => true } do
+  #   run "kill -s QUIT `cat tmp/pids/unicorn.pid`"
+  # end
+  
 end
 
-require 'capistrano-unicorn'
+# require 'capistrano-unicorn'
