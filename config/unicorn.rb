eb5004f2d8a27113d4ef4e5e0d2a5ce811dcf35f
Matt Elhotiby <hotiby@gmail.com>
2012-04-29 11:38:52 -0400

    fixing unicorn

--- config/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "/var/rails/mm/current"
-pid "/var/rails/mm/current/tmp/pids/unicorn.pid"
-stderr_path "/var/rails/mm/current/log/unicorn.log"
-stdout_path "/var/rails/mm/current/log/unicorn.log"
-
-listen "/tmp/unicorn.sock"
-worker_processes 2
-timeout 30
