464315745acbbd1cb4d062dbd24508b89c3ff957
Matt Elhotiby <hotiby@gmail.com>
2012-04-29 19:10:10 -0400

    adding the new pages and design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Muslim Marriage, Muslim Matrimonial, Muslim Singles, Muslim Dating, Muslima and Quran Dating at MuslimaLove.com"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
+  
+  def logo
+    image_tag("logo-big.png", alt: "MuslimaLove.com", title: "MuslimaLove.com")
+  end
+  
 end
