94d2ec529274fa4f951c2ff53d8a2ce9c9bd00a2
matt <elhotiby104@yahoo.com>
2012-05-04 16:56:38 -0400

    adding the new relationships to users

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
-gem "haml-rails"
+gem 'haml-rails'
 gem 'will_paginate', '3.0.3'
 
 group :assets do
@@ -16,11 +16,11 @@ gem 'unicorn'
 gem 'capistrano'
 gem 'execjs'
 gem 'therubyracer'
-gem "simple_form", "~> 2.0.2"
+gem 'simple_form', '~> 2.0.2'
 gem 'country_select'
-gem "paperclip", "~> 3.0"
-gem "devise"
-gem "geokit", "~> 1.6.5"
+gem 'paperclip', '~> 3.0'
+gem 'devise'
+gem 'geokit', '~> 1.6.5'
 gem 'bson_ext', '1.6.2'
 gem 'pg'
 
@@ -29,9 +29,9 @@ group :test do
   gem 'capybara'
   gem 'factory_girl_rails'
   gem 'database_cleaner'
-  gem "ruby-debug19"
+  gem 'ruby-debug19'
   gem 'database_cleaner'
-  gem "shoulda-matchers"
+  gem 'shoulda-matchers'
 end
 
 group :development do
@@ -42,5 +42,5 @@ group :development do
   gem 'guard-rspec'
   gem 'annotate'
   gem 'growl'
-  gem "shoulda-matchers"
+  gem 'shoulda-matchers'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,41 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :genre
 
   GENRE = ['Male', 'Female']
-  
+
   before_create :generate_lat_long
   before_update :generate_lat_long
-  
-  has_many :relationships
-  
+
+  has_many :relationships, foreign_key: 'follower_id',  dependent: :destroy
+  has_many :reverse_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   protected
-  
+
   def generate_lat_long
     require 'geo'
     lat, lng = Geo::get_coords("#{self.city} #{self.state}")
     self.latitude = lat
     self.longitude = lng
   end
-  
+
 end
