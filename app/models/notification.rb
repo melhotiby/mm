64716aaa35ab14f60b89eca366bca0e755835610
matt <hotiby@gmail.com>
2012-05-12 23:22:58 -0400

    adding the guard file update

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,4 @@
 class Notification < ActiveRecord::Base
   attr_accessible :freind_id, :message, :name, :notification_type_id, :user_id
+  belongs_to :user
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  # it { should have_many :relationships }
+   it { should have_many(:notifications) }
+   it { should have_many(:relationships) }
+   it { should have_many(:reverse_relationships) }
+   it { should have_many(:followed_users).through(:relationships) }
+   it { should have_many(:followers).through(:reverse_relationships) }
 end
